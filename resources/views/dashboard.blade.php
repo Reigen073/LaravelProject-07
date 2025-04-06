511017517ab7ff5c7827636fa8182897dbf569a2
Mathieu6798 <124693658+Mathieu6798@users.noreply.github.com>
2025-04-06 13:53:52 +0200

    Fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -39,7 +39,7 @@
             @auth
                 @if (auth()->user()->role === 'admin')
                     <a class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded text-sm"
-                       href="{{ route('returns.index') }}">
+                       href="{{ route('contracts.index') }}">
                         {{ __('messages.admin_contracts') }}
                     </a>
                 @endif
